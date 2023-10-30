7306c50d60b2830bb5914fbd4a7919c50a933f9c
ljdamkoehler <=>
2023-10-30 14:03:34 -0500

    find files1

---
+++ get_modfied_files.py
@@ -0,0 +1,20 @@
+import os
+import json
+import requests
+
+def get_modified_python_files():
+    event_path = os.environ['GITHUB_EVENT_PATH']
+    with open(event_path, 'r') as f:
+        event_data = json.load(f)
+
+    pr_url = event_data['pull_request']['url']
+    files_url = pr_url + '/files'
+    response = requests.get(files_url)
+    modified_files = [f['filename'] for f in response.json() if f['filename'].endswith('.py')]
+    return modified_files
+
+if __name__ == "__main__":
+    modified_files = get_modified_python_files()
+    print("Modified Python files found:")
+    for file in modified_files:
+        print(file)
