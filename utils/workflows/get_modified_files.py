c8f22fe4a9039434e9219d9f4ad851e87b7d3fee
ljdamkoehler <=>
2023-12-14 15:53:18 -0600

    all new21

--- utils/workflows/get_modified_files.py
+++ utils/workflows/get_modified_files.py
@@ -8,6 +8,7 @@ import requests
 
 def get_modified_python_files():
     event_path = os.environ['GITHUB_EVENT_PATH']
+    print('EVENT PATH!!!')
     print(event_path)
     with open(event_path, 'r') as f:
         event_data = json.load(f)
@@ -16,6 +17,7 @@ def get_modified_python_files():
     files_url = pr_url + '/files'
     print(files_url)
     response = requests.get(files_url)
+    print('THE JSON!!!')
     print(response.json())
     modified_files = [f['filename'] for f in response.json() if f['filename'].endswith('.py')]
     return modified_files
